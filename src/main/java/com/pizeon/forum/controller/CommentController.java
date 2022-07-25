b0de92deff152468ea1d123cab9cf5f775b5217f
주영 유 <qlenfrl0577@naver.com>
2022-07-25 21:10:36 +0900

    답글 생성 html 파일 수정(버그 발생)

--- src/main/java/com/pizeon/forum/controller/CommentController.java
+++ src/main/java/com/pizeon/forum/controller/CommentController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,12 +29,6 @@ public class CommentController {
 	
 	@Autowired
 	private PostRepository postRepository;
-	
-	@GetMapping("/createForm")
-	public String createCommentForm(@PathVariable String postId, Model model) {
-		model.addAttribute("postId", postId);
-		return "comment/createForm :: #create-comment-form";
-	}
 	
 	@PostMapping("/create")
 	public String create(@PathVariable String postId, HttpSession session, @RequestBody HashMap<String, Object> body, Model model) {
--- src/main/resources/static/js/post/show.js
+++ src/main/resources/static/js/post/show.js
@@ -1,10 +1,9 @@
-const onClickCreateBtn = (e, postId, logined_id) => {
-	e.preventDefault();
-				
-	if (!logined_id) {
+function createComment(postId, logined_id) {
+	return false;
+	/*if (!logined_id) {
 		alert("로그인 필요");
 	} else {
-		var description = e.target.description.value;
+		var description = document.getElementById("create-comment-desc").value;
 		
 		if (description === "") {
 			alert("내용이 없으면 안됨");
@@ -16,7 +15,7 @@ const onClickCreateBtn = (e, postId, logined_id) => {
 				},
 				body: JSON.stringify({
 				    userId: logined_id,
-					description: e.target.description.value
+					description: description
 				})
 			}).then(res => {
 				res.text().then(text => {
@@ -27,6 +26,8 @@ const onClickCreateBtn = (e, postId, logined_id) => {
 						newComment.setAttribute("class", "list-group-item");
 						newComment.innerHTML = text;
 						commentList.append(newComment);
+						
+						document.getElementById("create-comment-desc").value = "";
 					} else {
 						alert("다시 로그인 필요");
 					}
@@ -34,6 +35,8 @@ const onClickCreateBtn = (e, postId, logined_id) => {
 			})
 		}
 	}
+	
+	return false;*/
 }
 
 const onClickUpdateCommentBtn = (postId, commentId, loginedId) => {
@@ -48,14 +51,14 @@ const onClickUpdateCommentBtn = (postId, commentId, loginedId) => {
 		})
 	}).then(res => {
 		res.text().then(text => {
-			var newTag = document.createElement("div");
-			var comment = document.getElementById("comment-" + commentId);
-			
-			newTag.setAttribute("class", "update-comment");
-			newTag.setAttribute("id", "update-comment-" + commentId);
-			newTag.innerHTML = text;
-			
 			if (text) {
+				var newTag = document.createElement("div");
+				var comment = document.getElementById("comment-" + commentId);
+				
+				newTag.setAttribute("class", "update-comment");
+				newTag.setAttribute("id", "update-comment-" + commentId);
+				newTag.innerHTML = text;
+			
 				comment.append(newTag);
 			} else {
 				alert("오류 발생");
